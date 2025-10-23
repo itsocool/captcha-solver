fcaea2f87dd9a63fdd57ea3812803b8f471b8eda
th.kim <th.kim@hyperinfo.co.kr>
2025-10-23 10:13:27 +0900

    fix: 'kshop'에서 'default'로 captcha_id 변경 및 경고 필터링 로직 통합

--- keras_pred.py
+++ keras_pred.py
@@ -1,10 +1,8 @@
 import os
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # TensorFlow C++ 로그 레벨 (0=ALL,1=INFO,2=WARNING,3=ERROR)
 os.environ['GLOG_minloglevel'] = '2'  # Google 로깅
 
-import logging
-import warnings
+import logging, warnings
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 warnings.filterwarnings('ignore', category=FutureWarning)
 warnings.filterwarnings('ignore', category=DeprecationWarning)
@@ -13,7 +11,7 @@ from captchaResolver.keras_core import KerasModel
 from captchaResolver.engine import get_captcha_type_list
 from captchaResolver.keras_engine import predict, batch_predict_model
 
-captcha_id = 'kshop'
+captcha_id = 'default'
 captcha_type_list = get_captcha_type_list()
 train_data = captcha_type_list[captcha_id].train_data
 train_data.backend = 'keras'
--- keras_train.py
+++ keras_train.py
@@ -1,12 +1,17 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # TensorFlow 사용 시
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # TensorFlow C++ 로그 레벨 (0=ALL,1=INFO,2=WARNING,3=ERROR)
 os.environ['GLOG_minloglevel'] = '2'  # Google 로깅
+
+import logging, warnings
+logging.getLogger('tensorflow').setLevel(logging.ERROR)
+warnings.filterwarnings('ignore', category=FutureWarning)
+warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 from captchaResolver.engine import get_captcha_type_list
 from captchaResolver.keras_core import KerasModel
 from captchaResolver.keras_engine import train_model
 
-captcha_id = 'kshop'
+captcha_id = 'default'
 captcha_type_list = get_captcha_type_list()
 train_data = captcha_type_list[captcha_id].train_data
 train_data.backend = 'keras'
